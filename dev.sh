064cf72c4aebe51b6300c4df32431d3f25cc15be
James Parkhurst <james.parkhurst@diamond.ac.uk>
2019-08-01 10:55:49 +0100

    Some initial commits

---
+++ dev.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+function build_pybind {
+
+  git clone git@github.com:pybind/pybind11.git env/src/pybind11
+  pushd env/src/pybind11
+
+  cmake . -DCMAKE_INSTALL_PREFIX=../..
+  make
+  make install
+  python3 setup.py install
+
+  popd
+
+}
+
+function build_multem {
+
+  git clone git@github.com:jmp1985/MULTEM.git env/src/MULTEM
+
+}
+
+# Setup developement environment
+python3 -m venv env
+source env/bin/activate
+
+# Install the requirements
+pip install -r requirements.txt
+
+# Make a source directory
+mkdir -p env/src
+
+# Build dependancies
+build_pybind
+build_multem
